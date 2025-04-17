b66eb1d8840ba8d6605c5fffe6a96203cbcb73fa
somya <somyasharma0488@gmail.com>
2025-04-17 20:32:05 +0530

    main

---
+++ func.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int sum(int a, int b){
+    int Sum = a + b;
+
+    
+}
+
+
+int main(){
+    int a, b;
+    cout << "Enter a: "<<a<<endl;
+    cout<<"Enter b: "<< b<<endl;
+
+
+    cin >> a >> b;
+    int Sum=sum(a, b);
+
+    cout << "The sum of " << a << " and " << b << " is: " << a + b << endl;
+    
+
+}
---
+++ passByvalue.cpp
@@ -0,0 +1,16 @@
+void main(int num){
+    cout<<num<<endl;
+    num+=5;
+    cout<<num<<endl;
+    num+=5;
+    return 0;
+    // return 0; // This line is not necessary in C++ main function
+}
+
+int main(){
+    int num=10;
+    
+    main(num);
+    cout<<num<<endl;
+    return 0; // This line is not necessary in C++ main function
+}
