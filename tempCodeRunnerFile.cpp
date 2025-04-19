58cbff80693450fad4d9aadccb4a718fd76d5461
somya <somyasharma0488@gmail.com>
2025-04-19 13:24:56 +0530

    main

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,13 +1,21 @@
-int main(){
-//     int a;
-//     cin>>a;
-//     for (int i = 0; i < a; i++)
-//     {
-//         int n;
-//         cin>>n;
-//         cout<<"The value of n is: "<<n<<endl;
-//         tri(n);
-//     }
+#include<iostream>
+using namespace std;
+// Function to calculate x^n using recursion   
+   
+   
+   
+ int main(){
+    
+ 
+   int a;
+    cin>>a;
+    for (int i = 0; i < a; i++)
+    {
+        int n;
+        cin>>n;
+        cout<<"The value of n is: "<<n<<endl;
+        
+    }
     
     
-// }
+ }
