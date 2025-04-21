c110d3844b9b850bce5f704b399dfab9174c0f28
somya <somyasharma0488@gmail.com>
2025-04-21 21:54:00 +0530

    main

---
+++ chef.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Your code here
+    cout<<"I"<<endl<<"love"<<endl<<"Art"<<endl;
+    // cout<<"love"<<endl;
+    // cout<<"Art";
+
+    return 0;
+}
