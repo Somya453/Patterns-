e411de19ec1761c1e510b1c1f9f769c2694cba66
somya <somyasharma0488@gmail.com>
2025-04-22 22:34:26 +0530

    main

---
+++ pyramid pattern.cpp
@@ -0,0 +1,22 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int rows = 5;
+
+//     // first loop to print all rows
+//     for (int i = 0; i < rows; i++) {
+
+//         // inner loop 1 to print white spaces
+//         for (int j = 0; j < 2 * (rows - i) - 1; j++) {
+//             cout<<" ";
+//         }
+
+//         // inner loop 2 to print star numbers
+//         for (int k = 0; k < 2 * i + 1; k++) {
+//             cout<<("%d ", k + 1);
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
